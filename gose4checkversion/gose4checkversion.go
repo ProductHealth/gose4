8206c6489526fc6e6e61d31e331594a835aed07a
Niels Draaisma <niels.draaisma@producthealth.com>
2014-10-22 10:54:56 +0100

    Added -wait option to block until the version is deployed

--- gose4checkversion/gose4checkversion.go
+++ gose4checkversion/gose4checkversion.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/ProductHealth/gose4"
 )
@@ -16,6 +17,11 @@ import (
 func main() {
 	endpoint := flag.String("endpoint", "", "Full se4 status url")
 	expectedVersion := flag.String("version", "", "Version to validate")
+	duration := flag.Duration("wait", 0, "Max time to wait for server to report desired version")
+	interval := flag.Duration("interval", time.Second*10, "Time to wait between requests")
+
+	startTime := time.Now()
+
 	flag.Parse()
 	if *endpoint == "" {
 		log.Fatalf("endpoint parameter required")
@@ -23,26 +29,44 @@ func main() {
 	if *expectedVersion == "" {
 		log.Fatalf("version parameter required")
 	}
-
 	u, err := url.Parse(*endpoint)
+	for {
+		checkVersion(u, expectedVersion)
+		if duration.Seconds() == 0 {
+			log.Fatalf("Correct version not returned")
+		} else if time.Since(startTime).Seconds() > duration.Seconds() {
+			log.Fatalf("Correct version not returned within %v", duration)
+		} else {
+			log.Printf("Correct version not yet returned, waiting for %v", interval)
+			time.Sleep(*interval)
+		}
+	}
 	if err != nil {
 		log.Fatalf("Could not parse url %v", *endpoint)
 	}
+
+
+
+}
+
+func checkVersion(u *url.URL, expectedVersion *string) {
 	resp, err := http.Get(u.String())
 	if err != nil {
-		log.Fatalf("Encountered error while performing request : %v", err)
+		log.Printf("Encountered error while performing request : %v", err)
+		return
 	}
 	status := new(gose4.Status)
 	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("Could not read http response : %v", err)
+		log.Printf("Could not read http response : %v", err)
 	}
 	err = json.Unmarshal(bytes, status)
 	if err != nil {
-		log.Fatalf("Could not unmarshal json : %v", err)
+		log.Printf("Could not unmarshal json : %v", err)
 	}
 	if status.BuildNumber != *expectedVersion {
-		log.Fatalf("Running build number '%v' does not match expected '%v'", status.BuildNumber, *expectedVersion)
+		log.Printf("Running build number '%v' does not match expected '%v'", status.BuildNumber, *expectedVersion)
+	} else {
+		os.Exit(0)
 	}
-	os.Exit(0)
 }
